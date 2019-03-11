db8492826ff6d91e651888cb66fa9175f4208ded
Mini Beast <larisara.sat@mfu.ac.th>
2019-03-11 16:23:13 +0700

    show selected publication

--- headlines.py
+++ headlines.py
@@ -11,16 +11,16 @@ import urllib.parse
 app = Flask(__name__)
 app.config.from_object('config')
 
-RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
-             'cnn': 'http://rss.cnn.com/rss/edition.rss',
-             'fox': 'http://feeds.foxnews.com/foxnews/latest',
-             'iol': 'http://www.iol.co.za/cmlink/1.640'}
+RSS_FEEDS = {'BBC': 'http://feeds.bbci.co.uk/news/rss.xml',
+             'CNN': 'http://rss.cnn.com/rss/edition.rss',
+             'FOX': 'http://feeds.foxnews.com/foxnews/latest',
+             'IOL': 'http://www.iol.co.za/cmlink/1.640'}
              
 WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}"
 
 CURRENCY_URL = "https://openexchangerates.org//api/latest.json?app_id={}"
 
-DEFAULTS = {'publication':'bbc',
+DEFAULTS = {'publication':'BBC',
             'city': 'Chiang Rai,TH',
             'currency_from':'USD',
             'currency_to':'THB'}
@@ -28,7 +28,7 @@ DEFAULTS = {'publication':'bbc',
 @app.route("/",methods=['GET', 'POST'])
 def home():
     # get customized headlines, based on user input or default
-    publication = request.args.get('publication')
+    publication = request.args.get('publication').upper()
     if not publication:
         publication = DEFAULTS['publication']
     articles = get_news(publication)
@@ -48,6 +48,7 @@ def home():
           currency_to=DEFAULTS['currency_to']
     rate, currencies = get_rate(currency_from, currency_to)
     response = make_response(render_template("home.html",
+    publication = publication,
     articles=articles,
     weather=weather,
     currency_from=currency_from,
